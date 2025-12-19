08bfd8903a29ec3b2b5fe2f1cc528149ec35ae20
Andrew Farber <Andrew.Farber@outlook.com>
2025-12-19 12:38:37 -0800

    Bump home-manager state version

--- nixos/modules/home-manager/home.nix
+++ nixos/modules/home-manager/home.nix
@@ -8,7 +8,7 @@ in
 
   home.username = "${username}";
   home.homeDirectory = "/home/${username}";
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 
   imports = [
