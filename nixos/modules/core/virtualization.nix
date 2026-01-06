35313f5799e10ae91b28be734f746ac0c4d3bc61
Andrew Farber <Andrew.Farber@outlook.com>
2026-01-05 18:03:24 -0800

    Enable docker compose and docker buildx plugin

--- nixos/modules/core/virtualization.nix
+++ nixos/modules/core/virtualization.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   virtualisation = {
     docker.enable = true;
+    docker.extraPackages = [
+      pkgs.docker-buildx
+    ];
   };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
 }
