2024d0ad853bfe2d870cd23c56fddc4204cbfe1a
Andrew Farber <Andrew.Farber@outlook.com>
2026-01-19 16:11:35 -0800

    Conditional WLR_RENDERER setting

--- nixos/hosts/vbox-desktop/vbox.nix
+++ nixos/hosts/vbox-desktop/vbox.nix
@@ -1,12 +1,14 @@
-{ ... }:
+{ desktop, ... }:
 
 {
   virtualisation.virtualbox.guest.enable = true;
 
-  # VirtualBox lacks GPU acceleration for wlroots-based compositors (Sway).
-  # Force software rendering to fix greetd/sway render errors.
+  # VirtualBox environment variables for Wayland compositors.
+  # Sway supports pixman software rendering, but Hyprland requires OpenGL.
+  # For Hyprland, enable 3D Acceleration in VirtualBox VM settings.
   environment.sessionVariables = {
-    WLR_RENDERER = "pixman";
     WLR_NO_HARDWARE_CURSORS = "1";
-  };
+  } // (if desktop == "sway" then {
+    WLR_RENDERER = "pixman";
+  } else {});
 }
