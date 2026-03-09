82b2f835ac18edefa530fad5624cc780c891ad69
Andrew Farber <Andrew.Farber@outlook.com>
2026-03-08 18:43:06 -0700

    Revert networking changes

--- nixos/modules/core/network.nix
+++ nixos/modules/core/network.nix
@@ -3,21 +3,7 @@
 {
   networking.hostName = host;
   networking.firewall.enable = true;
-
-  # Use systemd-networkd instead of dhcpcd for DHCP.
-  # dhcpcd 10.3.0 has a bug in ipv6nd_expire causing repeated segfaults.
-  networking.useDHCP = false;
-  networking.useNetworkd = true;
-  systemd.network = {
-    enable = true;
-    networks."10-lan" = {
-      matchConfig.Type = "ether";
-      networkConfig = {
-        DHCP = "yes";
-        IPv6AcceptRA = true;
-      };
-    };
-  };
+  networking.firewall.trustedInterfaces = [ "docker0" ];
 
   services.openssh = {
     enable = true;
